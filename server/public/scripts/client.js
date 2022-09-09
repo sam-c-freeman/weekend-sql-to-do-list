8256f149bf7974f42b1e1399429a3d7f4b4f2a27
Sam Freeman <samcfreeman888@gmail.com>
2022-09-09 13:02:39 -0500

    pausing point. I want to create dummy db Data to check rendering function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,8 +3,23 @@ console.log( 'js' );
 $( document ).ready( function(){
   console.log( 'JQ' );
   // Establish Click Listeners
-//   setupClickListeners()
+  setupClickListeners()
   // load existing tasks on page load
 //   getTasks();
 
 });
+
+//on click, the addTask function will run to add task to database and eventually DOM
+function setupClickListeners (){
+    $('#addTaskButton').on('click', addTask);
+   
+}
+
+function addTask (){
+    let taskToAdd = {
+        task: $('#taskIn').val(),
+    }
+console.log(taskToAdd);
+ //need to add post route
+//clear input in then
+}
