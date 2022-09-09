0cd5b00d0f606d5bf88b701f2d096ac9c503f4c5
Sam Freeman <samcfreeman888@gmail.com>
2022-09-09 13:47:45 -0500

    paused.  Need to finish server side post route

--- server/routes/toDo.router.js
+++ server/routes/toDo.router.js
@@ -23,6 +23,33 @@ toDoRouter.get('/', (req, res) => {
 
 //Post Route to Add Task
 
+// koalaRouter.post('/', (req, res) => {
+//     const name = req.body.name;
+//     const gender = req.body.gender;
+//     const age = req.body.age;
+//     const readyForTransfer =req.body.readyForTransfer;
+//     const notes = req.body.notes;
+    
+//     const queryText = `
+//     INSERT INTO "koalas"
+//     ("name", "gender", "age", "readyForTransfer", "notes")
+//     VALUES
+//     ($1, $2, $3, $4, $5);
+//     `;
+
+//     const sqlValues = [name, gender, age, readyForTransfer, notes];
+   
+
+//     db.query(queryText, sqlValues)
+//         .then(result =>{
+//         res.send(200);
+//     })
+//     .catch(error =>{
+//         console.log('error adding Koalas', error);
+//         res.sendStatus(500);
+//     })
+// });
+
 //Put Route to mark as completed
 
 //Delete Route to Mark as Completed
