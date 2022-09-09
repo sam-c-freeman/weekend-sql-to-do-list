80d81e2fef191f8ce4af079de59acf7ac095533d
Sam Freeman <samcfreeman888@gmail.com>
2022-09-09 12:38:21 -0500

    all files linked and working.  Working on base page set up

---
+++ server/public/scripts/client.js
@@ -0,0 +1,10 @@
+console.log( 'js' );
+
+$( document ).ready( function(){
+  console.log( 'JQ' );
+  // Establish Click Listeners
+//   setupClickListeners()
+  // load existing tasks on page load
+//   getTasks();
+
+});
---
+++ server/routes/toDo.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const toDoRouter = express.Router();
+
+const db = require('../modules/pool');
+
+//Get Route to show current tasks
+
+//Post Route to Add Task
+
+//Put Route to mark as completed
+
+//Delete Route to Mark as Completed
+
+
+module.exports = toDoRouter;
