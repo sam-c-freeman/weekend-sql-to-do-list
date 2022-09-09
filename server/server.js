fae0e9146e5088924210a0faf2ff5a1030fa7fca
Sam Freeman <samcfreeman888@gmail.com>
2022-09-09 09:15:49 -0500

    set up router properly

--- server/server.js
+++ server/server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-
-//what does this need to be called?
-const Router = require('./routes/koala.router')
+const toDoRouter = require('./routes/toDo.router.js')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
 // ROUTES
-app.use('/koalas', koalaRouter)
+app.use('/tasks', toDoRouter)
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
